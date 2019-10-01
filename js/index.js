b3aeb2a766fdb829dfb18676e99f29e5866cb1b7
Pavelbure34 <dongbinsuh@gmail.com>
2019-10-01 15:48:34 -0400

    project init

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const nav = {
+    ot:()=>$("#main").html(data.ot),
+    day01:()=>$("#main").html(data.day01),
+    day02:()=>$("#main").html(data.day02),
+    day03:()=>$("#main").html(data.day03)
+}
